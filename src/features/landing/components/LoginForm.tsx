9e4683c8a75b0523c16c7aa9f1afbb20bf525332
Jim Yu <jimyu@multibase.com.hk>
2022-06-22 22:25:52 +0800

    landing page revamp

--- src/features/landing/components/LoginForm.tsx
+++ src/features/landing/components/LoginForm.tsx
@@ -1,6 +1,8 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
-import { Button, Paper, TextField } from "@mui/material";
+import { Button, Paper, TextField, Typography } from "@mui/material";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { css } from "@emotion/react";
 
 import "./LoginForm.css";
 
@@ -9,22 +11,46 @@ type Inputs = {
     password: string
 }
 
-export const LoginForm = () => {
+const loginFormContainer = css`
+width: 350px;
+margin: 100px auto 0;
+`;
+
+const loginFormInput = css`
+width: 300px;
+margin: 4px auto;
+`;
+
+const loginFormButton = css`
+width: 300px;
+margin: 4px auto;
+background-color: #fff;
+`;
+
+type data = {
+  title: string
+}
+
+export const LoginForm = (prop: data) => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
   return (
     <>
-      <div className="login-form-container">
+      <div
+        css={loginFormContainer}>
         <Paper>
+          <Typography>{prop.title}</Typography>
           <form id="login-form" onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <TextField {...register("id")} id="id" label="Login ID" variant="standard" />
+            <div css={loginFormInput}>
+              <TextField {...register("id")} id="id" label="Login ID" variant="standard" css={loginFormInput} />
             </div>
-            <div>
-              <TextField {...register("password")} id="password" label="Password" type="password" variant="standard" />
+            <div css={loginFormInput}>
+              <TextField {...register("password")} id="password" label="Password" type="password" variant="standard" css={loginFormInput} />
               {errors.password && <span>This field is required</span>}
             </div>
-            <Button type="submit">Login</Button>
+            <div css={loginFormInput}>
+              <Button type="submit" css={loginFormButton}>Login</Button>
+            </div>
           </form>
         </Paper>
       </div>
