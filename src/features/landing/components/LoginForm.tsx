b924db6707aaa6a7f4a0b8adcd2381aab849e715
Jim Yu <jimyu@multibase.com.hk>
2022-06-23 18:12:22 +0800

    update 20220623

--- src/features/landing/components/LoginForm.tsx
+++ src/features/landing/components/LoginForm.tsx
@@ -4,8 +4,6 @@ import { Button, Paper, TextField, Typography } from "@mui/material";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { css } from "@emotion/react";
 
-import "./LoginForm.css";
-
 type Inputs = {
     id: string,
     password: string
@@ -16,15 +14,25 @@ width: 350px;
 margin: 100px auto 0;
 `;
 
+const loginForm = css`
+padding: 10px;
+`;
+
 const loginFormInput = css`
 width: 300px;
 margin: 4px auto;
 `;
 
 const loginFormButton = css`
+color: white;
 width: 300px;
 margin: 4px auto;
-background-color: #fff;
+background-color: #fff0;
+background-image: linear-gradient(to right, #403a3e, #5790b8);
+`;
+
+const warning = css`
+color: #f00;
 `;
 
 type data = {
@@ -36,20 +44,20 @@ export const LoginForm = (prop: data) => {
   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
   return (
     <>
-      <div
-        css={loginFormContainer}>
-        <Paper>
+      <div css={loginFormContainer}>
+        <Paper css={loginForm}>
           <Typography>{prop.title}</Typography>
           <form id="login-form" onSubmit={handleSubmit(onSubmit)}>
             <div css={loginFormInput}>
-              <TextField {...register("id")} id="id" label="Login ID" variant="standard" css={loginFormInput} />
+              <TextField {...register("id", { required: true })} label="Login ID" variant="standard" css={loginFormInput} />
+              {errors.id && <span css={warning}>This field is required</span>}
             </div>
             <div css={loginFormInput}>
-              <TextField {...register("password")} id="password" label="Password" type="password" variant="standard" css={loginFormInput} />
-              {errors.password && <span>This field is required</span>}
+              <TextField {...register("password", { required: true })} label="Password" type="password" variant="standard" css={loginFormInput} />
+              {errors.password && <span css={warning}>This field is required</span>}
             </div>
             <div css={loginFormInput}>
-              <Button type="submit" css={loginFormButton}>Login</Button>
+              <Button type="submit" css={loginFormButton}>Sign In</Button>
             </div>
           </form>
         </Paper>
--- src/features/landing/routes/Landing.tsx
+++ src/features/landing/routes/Landing.tsx
@@ -4,7 +4,7 @@ import { LoginForm } from "../components/LoginForm";
 export const Landing = () => {
   return (
     <>
-      <LoginForm title="EVQ Sense Portal"/>
+      <LoginForm title="Portal"/>
     </>
   );
 };
